9a94e6fdcea1ba78a51c1530823ea6b5c89b1499
fazelginanda <fzginanda@gmail.com>
2024-11-25 20:31:10 +0700

    Adjustment: Move deriveConclusion function to test file

--- test.py
+++ test.py
@@ -1,18 +1,31 @@
 from rdr import Rule, Tree
 
 
+# Fungsi untuk mencari kesimpulan dari data berdasarkan tree
+def deriveConclusion(tree, data, conclusion):
+    if tree.root:
+        if tree.root.isSatisfied(data):
+            conclusion = tree.root.conclusion
+            if tree.right:
+                return deriveConclusion(tree.right, data, conclusion)
+        else:
+            if tree.left:
+                return deriveConclusion(tree.left, data, conclusion)
+    return conclusion
+
+
 # Tes fungsi Rule dan Tree
 def testRuleAndTree():
-    rule = Rule('A', 'X', ['P', 'Q'])
+    rule = Rule("A", "X", ["P", "Q"])
     tree = Tree(rule)
 
-    rule1 = Rule('B', 'Y', ['R'])
+    rule1 = Rule("B", "Y", ["R"])
     tree1 = Tree(rule1)
 
-    rule2 = Rule('C', 'Z', ['S'])
+    rule2 = Rule("C", "Z", ["S"])
     tree2 = Tree(rule2)
 
-    rule3 = Rule(['A', 'C'], 'Y', ['T'])
+    rule3 = Rule(["A", "C"], "Y", ["T"])
     tree3 = Tree(rule3)
 
     tree.left = tree1
@@ -29,52 +42,52 @@ def testInference():
     rule = Rule(True, "Sehat", [])
     tree = Tree(rule)
 
-    rule1 = Rule(['A', 'B'], 'X', ['A', 'B', 'C'])
+    rule1 = Rule(["A", "B"], "X", ["A", "B", "C"])
     tree1 = Tree(rule1)
     tree.right = tree1
 
-    rule2 = Rule(['D'], 'Y', ['A', 'B', 'D'])
+    rule2 = Rule(["D"], "Y", ["A", "B", "D"])
     tree2 = Tree(rule2)
     tree1.right = tree2
 
-    rule3 = Rule(['E'], 'Z', ['A', 'B', 'E'])
+    rule3 = Rule(["E"], "Z", ["A", "B", "E"])
     tree3 = Tree(rule3)
     tree2.left = tree3
 
-    rule4 = Rule(['C'], 'P', ['C', 'F'])
+    rule4 = Rule(["C"], "P", ["C", "F"])
     tree4 = Tree(rule4)
     tree1.left = tree4
 
-    rule5 = Rule(True, 'dX', ['A', 'B', 'C'])
+    rule5 = Rule(True, "dX", ["A", "B", "C"])
     tree5 = Tree(rule5)
     tree3.left = tree5
 
     tree.printPreorder()
 
-    data1 = ['A', 'B', 'C']
+    data1 = ["A", "B", "C"]
     print()
     print("Data: " + str(data1))
-    print("Kesimpulan: " + str(tree.deriveConclusion(data1, "")))
+    print("Kesimpulan: " + str(deriveConclusion(tree, data1, "")))
 
-    data2 = ['A', 'B', 'D']
+    data2 = ["A", "B", "D"]
     print()
     print("Data: " + str(data2))
-    print("Kesimpulan: " + str(tree.deriveConclusion(data2, "")))
+    print("Kesimpulan: " + str(deriveConclusion(tree, data2, "")))
 
-    data3 = ['A', 'B', 'E']
+    data3 = ["A", "B", "E"]
     print()
     print("Data: " + str(data3))
-    print("Kesimpulan: " + str(tree.deriveConclusion(data3, "")))
+    print("Kesimpulan: " + str(deriveConclusion(tree, data3, "")))
 
-    data4 = ['C', 'F']
+    data4 = ["C", "F"]
     print()
     print("Data: " + str(data4))
-    print("Kesimpulan: " + str(tree.deriveConclusion(data4, "")))
+    print("Kesimpulan: " + str(deriveConclusion(tree, data4, "")))
 
-    data5 = ['A', 'B', 'I']
+    data5 = ["A", "B", "I"]
     print()
     print("Data: " + str(data5))
-    print("Kesimpulan: " + str(tree.deriveConclusion(data5, "")))
+    print("Kesimpulan: " + str(deriveConclusion(tree, data5, "")))
 
 
 if __name__ == "__main__":
