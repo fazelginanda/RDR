e0e6fc7a1154fb9a6de98f2571a4e090d684d09a
fazelginanda <fzginanda@gmail.com>
2024-11-25 20:32:15 +0700

    Refactor: reformat rdr.py

--- rdr.py
+++ rdr.py
@@ -17,8 +17,13 @@ class Rule:
             return False
 
     def print(self):
-        print(str(self.condition) + " -> " + self.conclusion +
-              " | Kasus Kunci: " + str(self.case))
+        print(
+            str(self.condition)
+            + " -> "
+            + self.conclusion
+            + " | Kasus Kunci: "
+            + str(self.case)
+        )
 
 
 class Tree:
@@ -26,17 +31,6 @@ class Tree:
         self.root = root
         self.left = None
         self.right = None
-
-    def deriveConclusion(self, data, conclusion):
-        if self.root:
-            if self.root.isSatisfied(data):
-                conclusion = self.root.conclusion
-                if self.right:
-                    return self.right.deriveConclusion(data, conclusion)
-            else:
-                if self.left:
-                    return self.left.deriveConclusion(data, conclusion)
-        return conclusion
 
     def search(self, data, tree):
         if self.root:
@@ -73,11 +67,10 @@ def loadTreeFromPickle(file_path):
     try:
         # Periksa apakah ada file yang ingin dicari
         if not os.path.exists(file_path):
-            raise FileNotFoundError(
-                f"File '{file_path}' tidak ditemukan.")
+            raise FileNotFoundError(f"File '{file_path}' tidak ditemukan.")
 
         # Buka dan load file pickle
-        with open(file_path, 'rb') as file:
+        with open(file_path, "rb") as file:
             data = pickle.load(file)
         return data
     except FileNotFoundError as e:
@@ -96,7 +89,7 @@ if __name__ == "__main__":
         print("1. Membuat tree RDR baru")
         print("2. Menggunakan RDR yang sudah disimpan")
         inputRDR = input("Jawaban: ")
-        if (inputRDR == '1' or inputRDR == '2'):
+        if inputRDR == "1" or inputRDR == "2":
             break
         else:
             print("Jawaban tidak valid. Masukkan 1 atau 2")
@@ -105,7 +98,7 @@ if __name__ == "__main__":
     tree = None
 
     # Membuat tree RDR baru
-    if inputRDR == '1':
+    if inputRDR == "1":
         # Buat tree dengan menginisialisasi root yang berisi kesimpulan default
         print()
         print("Buat Tree RDR Baru")
@@ -115,9 +108,8 @@ if __name__ == "__main__":
         tree = Tree(defaultRule)
 
     # Menggunakan RDR yang sudah disimpan
-    elif inputRDR == '2':
-        treeFilePath = input(
-            "Masukkan path dari file tree yang akan digunakan: ")
+    elif inputRDR == "2":
+        treeFilePath = input("Masukkan path dari file tree yang akan digunakan: ")
         tree = loadTreeFromPickle(treeFilePath)
 
     if tree is not None:
@@ -131,13 +123,13 @@ if __name__ == "__main__":
             print("1. Ya")
             print("2. Tidak")
             inputNewData = input("Jawaban: ")
-            if (inputNewData == '1' or inputNewData == '2'):
+            if inputNewData == "1" or inputNewData == "2":
                 break
             else:
                 print("Jawaban tidak valid. Masukkan 1 atau 2")
 
         # Jika pakar ingin memasukkan data baru, maka baca data dan lakukan inferensi
-        while inputNewData == '1':
+        while inputNewData == "1":
             # Baca data baru dari masukan pengguna
             print()
             data = input("Masukkan data: ")
@@ -153,36 +145,41 @@ if __name__ == "__main__":
                 print("1. Setuju")
                 print("2. Tidak Setuju")
                 feedback = input("Jawaban: ")
-                if (feedback == '1' or feedback == '2'):
+                if feedback == "1" or feedback == "2":
                     break
                 else:
                     print("Jawaban tidak valid. Masukkan 1 atau 2")
 
             # Jika pengguna pakar tidak setuju, maka buat rule baru
             # Cari rule terakhir yang dipenuhi pada jalur inferensi yang menghasilkan klasifikasi yang salah
-            if feedback == '2':
+            if feedback == "2":
                 print()
                 print("Manipulasi pohon RDR")
                 print("Data saat ini: " + str(data))
-                print("Kasus kunci yang terakhir dipenuhi: " +
-                      str(foundTree.root.case))
+                print("Kasus kunci yang terakhir dipenuhi: " + str(foundTree.root.case))
 
                 # Cari kondisi yang ada pada data saat ini dan tidak ada pada kasus kunci terakhir
                 difference = [
-                    condition for condition in data if condition not in foundTree.root.case]
+                    condition
+                    for condition in data
+                    if condition not in foundTree.root.case
+                ]
                 print(
-                    "Selisih antara data saat ini dengan kasus kunci terakhir: " + str(difference))
+                    "Selisih antara data saat ini dengan kasus kunci terakhir: "
+                    + str(difference)
+                )
 
                 # Jika selisih tidak kosong, maka tanya pakar apa kondisi yang dipilih untuk rule baru
                 if difference:
                     print()
                     print(
-                        "Pilih kondisi untuk rule baru. Kondisi harus merupakan bagian dari selisih yang ditampilkan sebelumnya.")
+                        "Pilih kondisi untuk rule baru. Kondisi harus merupakan bagian dari selisih yang ditampilkan sebelumnya."
+                    )
                     while True:
-                        newCondition = input(
-                            "Masukkan kondisi untuk rule baru: ")
-                        newCondition = [condition.strip()
-                                        for condition in newCondition.split(",")]
+                        newCondition = input("Masukkan kondisi untuk rule baru: ")
+                        newCondition = [
+                            condition.strip() for condition in newCondition.split(",")
+                        ]
                         if set(newCondition).issubset(set(difference)):
                             break
                         else:
@@ -190,7 +187,8 @@ if __name__ == "__main__":
                 # Jika selisih kosong, maka kondisi adalah True. Pakar ingin merevisi keputusan yang telah dibuat sebelumnya
                 else:
                     print(
-                        "Kondisi rule baru adalah True. Anda merevisi keputusan yang telah Anda buat sebelumnya.")
+                        "Kondisi rule baru adalah True. Anda merevisi keputusan yang telah Anda buat sebelumnya."
+                    )
                     newCondition = True
 
                 # Tanya pakar apa kesimpulan untuk rule baru
@@ -217,7 +215,7 @@ if __name__ == "__main__":
                 print("1. Ya")
                 print("2. Tidak")
                 inputNewData = input("Jawaban: ")
-                if (inputNewData == '1' or inputNewData == '2'):
+                if inputNewData == "1" or inputNewData == "2":
                     break
                 else:
                     print("Jawaban tidak valid. Masukkan 1 atau 2")
@@ -229,22 +227,20 @@ if __name__ == "__main__":
             print("1. Ya")
             print("2. Tidak")
             inputSave = input("Jawaban: ")
-            if (inputSave == '1' or inputSave == '2'):
+            if inputSave == "1" or inputSave == "2":
                 break
             else:
                 print("Jawaban tidak valid. Masukkan 1 atau 2")
 
         # Jika pengguna ingin menyimpan tree, simpan tree sebagai file Pickle
-        if inputSave == '1':
+        if inputSave == "1":
             print()
-            if inputRDR == '1':
+            if inputRDR == "1":
                 print("File akan disimpan pada current directory")
-                newTreeFilename = input(
-                    "Masukkan nama file tree tanpa extension: ")
+                newTreeFilename = input("Masukkan nama file tree tanpa extension: ")
                 newTreeFilePath = newTreeFilename + ".pkl"
             else:
                 newTreeFilePath = treeFilePath
             with open(newTreeFilePath, "wb") as f:
                 pickle.dump(tree, f)
-            print("File " + newTreeFilePath +
-                  " telah berhasil disimpan")
+            print("File " + newTreeFilePath + " telah berhasil disimpan")
